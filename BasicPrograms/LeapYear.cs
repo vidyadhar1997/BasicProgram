c6a62f8df7bc097ec0e763568290d6fc5f5fdc88
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-02 12:34:35 +0530

    [vidyadhar hudge]Add:leap year program

---
+++ BasicPrograms/LeapYear.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicPrograms
+{
+    class LeapYear
+    {
+        public static void leapYear()
+        {
+            Console.WriteLine("Enter the year");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+            {
+                Console.WriteLine("year is leap year");
+            }
+            else
+            {
+                Console.WriteLine("year is not leap year");
+            }
+            Console.ReadLine();
+        }
+    }
+}
