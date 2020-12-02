93d041d4774242fc61bda0e55c41bde40e0df13e
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-02 12:02:02 +0530

    [vidyadhar hudge]Add:Welcome message

---
+++ BasicPrograms/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BasicPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("press 1 for userName,press 2 for addtionOfTwoNumber,press 3 for leapYear,press 4 for powerOfTwo");
+            int choise = Convert.ToInt32(Console.ReadLine());
+            switch (choise)
+            {
+                case 1:
+                    UserName.username();
+                    break;
+                case 2:
+                    AddtionOfTwoNumber.addtionOfTwoNumber();
+                    break;
+                case 3:
+                    LeapYear.leapYear();
+                    break;
+                case 4:
+                    PowerOfTwo.powerOfTwo();
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+    }
+}
