3f9845b756a2c799c52c616c913fd6c6bd8695ac
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-02 12:37:59 +0530

    [vidyadhar hudge]Add:power of two program

---
+++ BasicPrograms/PowerOfTwo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicPrograms
+{
+    class PowerOfTwo
+    {
+        public static void powerOfTwo()
+        {
+            Console.WriteLine("Enter the number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            double powerOfNumber = Math.Pow(number, 2);
+            Console.WriteLine("power of number is" + powerOfNumber);
+            Console.ReadLine();
+        }
+    }
+}
