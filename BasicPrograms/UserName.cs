c4574157b7b37e7dc550d8ee77e644c902d7bb0f
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-02 12:20:28 +0530

    [vidyadhar hudge]Add:User name

---
+++ BasicPrograms/UserName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicPrograms
+{
+    class UserName
+    {
+        public static void username()
+        {
+            Console.WriteLine("Enter user name");
+            String user = Console.ReadLine();
+            Console.WriteLine("user name is " + user);
+        }
+    }
+}
+
