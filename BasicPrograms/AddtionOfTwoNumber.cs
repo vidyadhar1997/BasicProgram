758d035327f17651ecbcc1621a2af7d9c70cf489
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-02 12:32:37 +0530

    [vidyadhar hudge]Add:addtion of two number

---
+++ BasicPrograms/AddtionOfTwoNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicPrograms
+{
+    class AddtionOfTwoNumber
+    {
+        public static void addtionOfTwoNumber()
+        {
+            Console.WriteLine("Enter the first number");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int result = number1 + number2;
+            Console.WriteLine("Addtion of two number is " + result.ToString());
+            Console.ReadLine();
+        }
+    }
+}
